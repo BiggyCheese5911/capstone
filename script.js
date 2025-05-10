334b32b40eb73d2e61eaf93265f2d7f10f5531e2
BiggyCheese5911 <damian.edgar1230@gmail.com>
2025-05-09 17:26:15 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,61 @@
 let inventory = [];
 
-function addInventory(){
+function addInventory() {
+  if (
+    document.getElementById("apn").value &&
+    document.getElementById("cpt").value &&
+    document.getElementById("cip").value
+  ) {
     document.getElementById("show-inventory").style.display = "none";
 
     let newItem = {
-        name: document.getElementById('apn').value,
-        productType: document.getElementById('cpt').value,
-        itemPrice: document.getElementById('cip').value
-    }
+        id: Date.now(),
+      time: new Date().toLocaleDateString("en-us", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+      }),
+      name: document.getElementById("apn").value,
+      productType: document.getElementById("cpt").value,
+      itemPrice: document.getElementById("cip").value,
+    };
 
     console.log(newItem);
     inventory.push(newItem);
     console.log(inventory);
-    document.getElementById('aci').innerHTML = "";
-
-    if(document.getElementById('apn').value && document.getElementById('cpt').value && document.getElementById('cip').value){inventory.forEach(item => {
-        let card = document.createElement('div');
-        let btn = document.createElement('button')
-        // card.innerHTML = item.name + ' ' + item.productType + ' ' + '$' + item.itemPrice;
-        card.innerHTML = `${item.name} ${item.productType} $${item.itemPrice} <button class="add-btn" onclick="dlt()">Delete</button>`;
-        card.classList.add("new-item");
-        document.getElementById('aci').append(card);
-    })}
+    display(inventory)
+  }
 }
 
-function dlt(){
-    inventory.splice();
+function filter(category) {
+    console.log(category);
+    let filtered = inventory.filter(item => {
+        return item.productType.includes(category)
+    });
+    console.log(filtered);
+
+    display(filtered)
+};
+
+function display(items){
+    document.getElementById("aci").innerHTML = "";
+    items.forEach((item) => {
+        let card = document.createElement("div");
+        let btn = document.createElement("button");
+        // card.innerHTML = item.name + ' ' + item.productType + ' ' + '$' + item.itemPrice;
+        card.innerHTML = `Item Name: ${item.name} | Item Type: ${item.productType} | Item Price: $${item.itemPrice} | Entered On: ${item.time} <button class="add-btn" onclick="dlt(${item.id})">Delete</button>`;
+        card.classList.add("new-item");
+        document.getElementById("aci").append(card);
+      });
+}
+
+function dlt(id){
+    // inventory.splice(index, 1)
+    let newIn = inventory.filter(item => item.id = id)
+    console.log(newIn)
+    display(inventory)
+
+    filter()
 }
